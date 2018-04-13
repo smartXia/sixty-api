af69886a5faae04c5528ada7d5946d270a9b5d71
Liulin <liulin@corp-ci.com>
2018-04-13 09:34:22 +0800

    获取文章数据

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=101.200.52.143;dbname=yii',
+    'dsn' => 'mysql:host=101.200.52.143;dbname=sixty',
     'username' => 'root',
     'password' => '60+Hzjswlgbsj',
     'charset' => 'utf8',
